fbea463f469e6f3e50c55d3aa06ec4f906ab53b3
Abdul Rehman <137405437+AbdulRehman938@users.noreply.github.com>
2025-09-19 17:10:49 +0500

    commit27

--- src/components/Home/Mirror.jsx
+++ src/components/Home/Mirror.jsx
@@ -126,21 +126,21 @@ const Mirror = () => {
             viewport={{ once: false, amount: 0.2 }}
           >
             <div className="w-full md:w-1/2 flex flex-col gap-4 text-center md:text-left px-2 xl:mt-[0rem] xl:ml-[8rem] lg:ml-[5rem] 2xl:ml-[10rem] 2xl:mt-[1rem] 3xl:ml-[15rem] 3xl:mt-[1rem] xxs:w-[100vw] ">
-              <h1 className="3xl:text-7xl 3xl:ml-10 2xl:text-6xl xl:text-5xl lg:text-5xl md:text-4xl xs:text-3xl text-primary font-bold xss:text-3xl xss:w-[100vw] xss:ml-[-2.5rem]">
+              <h1 className="3xl:text-7xl 3xl:ml-10 2xl:text-6xl xl:text-5xl lg:text-5xl md:text-4xl xs:text-3xl text-primary font-bold xss:text-4xl xss:w-[100vw] xss:ml-[-2.5rem]">
                 We've all been there
               </h1>
               <p className="3xl:ml-[2.5rem] 3xl:w-[33rem] 2xl:text-2xl xl:text-xl lg:text-lg md:text-base sm:text-sm xs:text-xl text-gray-500 font-medium xss:w-[100vw] xss:ml-[-2.5rem]">
                 You're away from home and unsure <br className="xs:hidden" /> where you can <span className="text-black font-medium">go</span>.
               </p>
             </div>
-            <div className="w-full md:w-1/2 flex justify-center md:justify-end xl:mt-[5rem] lg:mt-[2rem] 2xl:mt-[6rem] 3xl:mt-[6rem] ">
+            <div className="w-full md:w-1/2 flex justify-center md:justify-end xl:mt-[5rem] lg:mt-[2rem] 2xl:mt-[6rem] 3xl:mt-[6rem] xss:mt-[-2rem] ">
               <img src="/Home-character2.svg" alt="char2" className="w-2/3 xs:w-[60%] lg:w-[55%] xl:w-[50%] 2xl:w-[50%] 3xl:w-[60%] max-w-md" />
             </div>
           </motion.div>
 
           {/* Section 2 */}
           <motion.div
-            className="relative z-30 w-full flex flex-col md:flex-row-reverse items-center md:items-start justify-between gap-10 py-24 xs:relative xs:top-[-25rem] xl:mt-[5rem] 3xl:mt-[6rem] xss:mt-[-40rem] "
+            className="relative z-30 w-full flex flex-col md:flex-row-reverse items-center md:items-start justify-between gap-10 py-24 xs:relative xs:top-[-25rem] xl:mt-[5rem] 3xl:mt-[6rem] xss:mt-[-44rem]"
             variants={fadeVariant}
             initial="hidden"
             whileInView="visible"
@@ -148,7 +148,7 @@ const Mirror = () => {
             viewport={{ once: false, amount: 0.2 }}
           >
             <div className="w-full md:w-1/2 flex flex-col gap-4 text-center md:text-left px-2 2xl:w-[40%] xl:mr-[5rem] xl:mt-[-8rem] lg:mt-[-5rem] 3xl:mr-[8rem] xss:w-[100vw]">
-              <h1 className="2xl:text-6xl lg:text-5xl md:text-4xl sm:text-3xl  text-primary font-bold xs:text-3xl xss:text-3xl">
+              <h1 className="2xl:text-6xl lg:text-5xl md:text-4xl sm:text-3xl  text-primary font-bold xs:text-3xl xss:text-4xl">
                 A tiny ask shouldn't <br /> feel so big.
               </h1>
               <p className="2xl:text-2xl xl:text-sm lg:text-lg md:text-base sm:text-sm xs:text-xs text-gray-500 font-medium xss:w-[100vw] xss:text-sm">
@@ -157,14 +157,14 @@ const Mirror = () => {
                 want to tell someone I have to evacuate <br className="xs:hidden" /> my bowels.
               </p>
             </div>
-            <div className="w-full md:w-1/2 flex justify-center md:justify-start xl:ml-[3rem] lg:mt-[-1rem] 2xl:mt-[-2rem] 3xl:ml-[5rem] 3xl:mt-[-3rem] ">
+            <div className="w-full md:w-1/2 flex justify-center md:justify-start xl:ml-[3rem] lg:mt-[-1rem] 2xl:mt-[-2rem] 3xl:ml-[5rem] 3xl:mt-[-3rem] xss:mt-[-3rem] ">
               <img src="/Home-character3.svg" alt="char3" className="w-2/3 sm:w-1/2 lg:w-[45%] lg:ml-[10rem] xl:w-[40%] 2xl:w-[35%] 2xl:ml-[10rem] 2xl:mt-[5rem] max-w-md" />
             </div>
           </motion.div>
 
           {/* Section 3 */}
           <motion.div
-            className="relative z-30 w-full flex flex-col md:flex-row items-center md:items-start justify-between gap-10 py-24 xl:mt-[-25rem] 2xl:mt-[-20rem] 2xl:px-[5rem] 3xl:mt-[-15rem] lg:mt-[-30rem] lg:pr-[5rem] xss:mt-[-8rem]"
+            className="relative z-30 w-full flex flex-col md:flex-row items-center md:items-start justify-between gap-10 py-24 xl:mt-[-25rem] 2xl:mt-[-20rem] 2xl:px-[5rem] 3xl:mt-[-15rem] lg:mt-[-30rem] lg:pr-[5rem] xss:mt-[-10rem]"
             variants={fadeVariant}
             initial="hidden"
             whileInView="visible"
@@ -172,7 +172,7 @@ const Mirror = () => {
             viewport={{ once: false, amount: 0.2 }}
           >
             <div className="w-full md:w-1/2 flex flex-col gap-4 text-center md:text-left px-2 2xl:mt-[-10rem] xl:mt-[-7rem] 3xl:ml-[10rem] lg:mt-[-4rem] lg:ml-[8rem] ">
-              <h1 className="3xl:text-6xl 2xl:text-7xl xl:text-6xl lg:text-5xl md:text-4xl sm:text-3xl xs:text-2xl text-primary font-bold xss:text-3xl">
+              <h1 className="3xl:text-6xl 2xl:text-7xl xl:text-6xl lg:text-5xl md:text-4xl sm:text-3xl xs:text-2xl text-primary font-bold xss:text-4xl">
                 No more asking permission
               </h1>
               <p className="2xl:text-2xl xl:text-xl lg:text-lg md:text-base sm:text-sm xs:text-xs text-gray-500 font-medium">
@@ -181,7 +181,7 @@ const Mirror = () => {
                 unlock to find <span className="text-black font-medium">relief</span>.
               </p>
             </div>
-            <div className="w-full md:w-1/2 flex justify-center md:justify-end 3xl:mr-[10rem] lg:mt-[4rem] 2xl:mt-[-1rem] 3xl:mt-[1rem] ">
+            <div className="w-full md:w-1/2 flex justify-center md:justify-end 3xl:mr-[10rem] lg:mt-[4rem] 2xl:mt-[-1rem] 3xl:mt-[1rem] xss:mt-[-2rem] ">
               <img src="/Home-character4.svg" alt="char4" className="w-2/3 sm:w-1/2 lg:w-[60%] xl:w-[50%] 2xl:w-[45%] 2xl:mt-10 3xl:w-[55%] max-w-md xss:w-[65%]" />
             </div>
           </motion.div>
