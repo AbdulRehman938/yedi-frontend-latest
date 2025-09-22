055dc5362a7c9011349d8f1152fe6abc509c31b8
Abdul Rehman <137405437+AbdulRehman938@users.noreply.github.com>
2025-09-22 18:20:52 +0500

    commit46

--- src/components/Home/Mirror.jsx
+++ src/components/Home/Mirror.jsx
@@ -57,7 +57,7 @@ const Mirror = () => {
   }, [])
 
   return (
-    <div className="w-full bg-secondary mt-[10rem] flex flex-col items-center relative pt-56 overflow-visible 2xl:mt-[30rem] lg:mt-[15rem] xl:mt-10rem] lg:pt-[10rem] 3xl:mt-[40rem]">
+    <div className="w-full bg-secondary mt-[10rem] flex flex-col items-center relative pt-56 overflow-visible 2xl:mt-[30rem] lg:mt-[15rem] xl:mt-[15rem] lg:pt-[10rem] 3xl:mt-[40rem]">
       <div className="w-full relative flex flex-col items-center px-4 sm:px-6 lg:px-8 overflow-visible">
         {/* Top overflowing mirror image */}
         <motion.img
