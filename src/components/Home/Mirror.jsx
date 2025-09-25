e5a1783dc80411ad94466328cf49cc5351c81c35
Abdul Rehman <137405437+AbdulRehman938@users.noreply.github.com>
2025-09-25 17:56:34 +0500

    commit138

--- src/components/Home/Mirror.jsx
+++ src/components/Home/Mirror.jsx
@@ -41,7 +41,7 @@ const Mirror = () => {
   return (
     <>
       {/* Desktop version */}
-      <div className="hidden sm:block w-full bg-secondary 3xl:h-[210vh] xl:h-[332vh] lg:h-[240vh] flex-col items-center relative pt-56 overflow-visible 2xl:mt-[30rem] lg:mt-[30rem] lg:pt-[10rem] 3xl:mt-[40rem]">
+      <div className="hidden sm:block w-full bg-secondary 3xl:h-[210vh] xl:h-[340vh] lg:h-[240vh] flex-col items-center relative pt-56 overflow-visible 2xl:mt-[30rem] lg:mt-[30rem] lg:pt-[10rem] 3xl:mt-[40rem]">
         <div className="w-full relative flex flex-col items-center px-4 sm:px-6 lg:px-8 overflow-visible">
           {/* Top overflowing mirror image */}
           <motion.img
@@ -107,7 +107,7 @@ const Mirror = () => {
 
             {/* Section 1 */}
             <motion.div
-              className="relative z-30 w-[80%] ml-28 flex flex-col md:flex-row items-center md:items-start justify-between gap-10 mt-[10rem] 3xl:mt-48 xs:ml-[3.5rem] xs:relative xs:top-[-30rem] lg:mt-[-1rem] xl:mt-[36rem]"
+              className="relative z-30 w-[80%] ml-28 flex flex-col md:flex-row items-center md:items-start justify-between gap-10 mt-[10rem] 3xl:mt-48 xs:ml-[3.5rem] xs:relative xs:top-[-30rem] lg:mt-[-1rem] xl:mt-[35rem] 2xl:mt-[5rem]"
               variants={fadeVariant}
               initial="hidden"
               whileInView="visible"
@@ -160,7 +160,7 @@ const Mirror = () => {
               exit="exit"
               viewport={{ once: false, amount: 0.2 }}
             >
-              <div className="w-full md:w-1/2 flex flex-col gap-4 text-center md:text-left px-2 lg:mt-[1rem] 3xl:mt-[10rem] xl:mt-[5.5rem] 3xl:ml-[10rem] lg:ml-[8rem] ">
+              <div className="w-full md:w-1/2 flex flex-col gap-4 text-center md:text-left px-2 lg:mt-[1rem] 3xl:mt-[10rem] xl:mt-[5rem] 3xl:ml-[10rem] lg:ml-[8rem] ">
                 <h1 className="3xl:text-6xl 2xl:text-7xl xl:text-6xl lg:text-5xl md:text-4xl sm:text-3xl xs:text-2xl text-primary font-bold">
                   No more asking permission
                 </h1>
