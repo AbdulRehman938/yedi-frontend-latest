958d5b2f868bc402dd35c70f6f82a81631ccb986
Abdul Rehman <137405437+AbdulRehman938@users.noreply.github.com>
2025-09-29 10:26:44 +0500

    commit1

---
+++ src/components/Locations/Cities.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cities = () => {
+  return (
+    <div className='w-full h-[100%] bg-transparent flex flex-col items-center justify-start'>Cities</div>
+  )
+}
+
+export default Cities
--- src/pages/Locations.jsx
+++ src/pages/Locations.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Locations/Hero'
+import Cities from '../components/Locations/Cities'
 
 const Locations = () => {
   return (
-    <div>
+    <div className='w-[100vw] h-[200rem] bg-transparent flex flex-col items-center justify-start'>
       <Hero />
+      <Cities />
     </div>
   )
 }
